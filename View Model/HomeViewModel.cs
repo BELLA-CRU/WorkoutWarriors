21c8059069a6e837b5884f2112d41139cadab694
BELLA-CRU <89539211+BELLA-CRU@users.noreply.github.com>
2023-04-19 16:46:20 -0500

    Added IP address locator, fixed unit test

---
+++ View Model/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using WorkoutWarriors.Models;
+
+namespace WorkoutWarriors.View_Model
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Gym> Gyms { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+
+
+
+    }
+}
--- WorkoutWarriorsWebApp.Tests/GymControllerTests.cs
+++ WorkoutWarriorsWebApp.Tests/GymControllerTests.cs
@@ -2,16 +2,17 @@ using FakeItEasy;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using WorkoutWarriorsWebApp.Controllers;
-using WorkoutWarriorsWebApp.Interfaces;
-using WorkoutWarriorsWebApp.Models;
-using WorkoutWarriorsWebApp.Services;
+using WorkoutWarriors.Controllers;
+using WorkoutWarriors.Data.Interfaces;
+using WorkoutWarriors.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using WorkoutWarriors.Controllers;
+using WorkoutWarriors.Data.Interfaces;
 
 namespace WorkoutWarriorsWebApp.Tests.Controller
 {
@@ -19,17 +20,16 @@ namespace WorkoutWarriorsWebApp.Tests.Controller
     {
         private GymController _gymController;
         private IGymRepository _gymRepository;
-        private IPhotoService _photoService;
+
         private IHttpContextAccessor _httpContextAccessor;
         public GymControllerTests()
         {
             //Dependencies
             _gymRepository = A.Fake<IGymRepository>();
-            _photoService = A.Fake<IPhotoService>();
             _httpContextAccessor = A.Fake<HttpContextAccessor>();
 
             //SUT
-            _gymController = new GymController(_gymRepository, _photoService);
+            _gymController = new GymController(_gymRepository, _httpContextAccessor);
         }
 
         [Fact]
@@ -44,18 +44,7 @@ namespace WorkoutWarriorsWebApp.Tests.Controller
             result.Should().BeOfType<Task<IActionResult>>();
         }
 
-        [Fact]
-        public void WarriorsController_Detail_ReturnsSuccess()
-        {
-            //Arrange
-            var id = 1;
-            var gym = A.Fake<Gym>();
-            A.CallTo(() => _gymRepository.GetByIdAsync(id)).Returns(gym);
-            //Act
-            var result = _gymController.DetailGym(id, "Gym");
-            //Assert
-            result.Should().BeOfType<Task<IActionResult>>();
-        }
+      
 
 
     }
